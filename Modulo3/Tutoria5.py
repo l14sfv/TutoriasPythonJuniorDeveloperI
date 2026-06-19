b6be0c72fda6ebf111e53c8effd9868ed377c218
l14sfv <l14sfp@gmail.com>
2026-06-18 20:33:59 -0500

    18junio2026

---
+++ Modulo3/Tutoria5.py
@@ -0,0 +1,15 @@
+# comando para subir carpeta clonada en un nuevo repo
+
+#eliminar cahce y submodulos de .git
+# # git rm --cached mental-code-c7
+# # rm -rf mental-code-c7/.git
+# # rm -f .gitmodules
+
+# #eliminar URL actual
+# git remote remove origin
+
+# #agregar URL nueva
+# git remote add origin https://github.com/l14sfv/nuevacarpeta.git
+
+# #verificar la URL nueva
+# git remote -v
